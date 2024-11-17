06d5cf37166454a6cef934749061002d58a124e6
Edgar Castro <edgar.castro.vm@gmail.com>
2024-11-17 13:40:14 -0500

    Se agrega constructores

--- uisrael-lib-utils/src/main/java/ec/edu/uisrael/dto/Product.java
+++ uisrael-lib-utils/src/main/java/ec/edu/uisrael/dto/Product.java
@@ -1,10 +1,14 @@
 package ec.edu.uisrael.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Product {
     private String type;
     private String name;
