0bb6bc53e30afb7910ee73cc6b9f3597709aa79f
Pablo Peñaloza <111254304+PablinP@users.noreply.github.com>
2024-11-16 23:00:56 -0500

    Crud product
    crud product

--- api-person/src/main/java/ec/edu/uisrael/apiperson/controller/PersonController.java
+++ api-person/src/main/java/ec/edu/uisrael/apiperson/controller/PersonController.java
@@ -1,15 +1,14 @@
 package ec.edu.uisrael.apiperson.controller;
 
 import ec.edu.uisrael.apiperson.data.FakeData;
+import ec.edu.uisrael.apiperson.data.Person;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.github.javafaker.Faker;
-
 import java.util.HashMap;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping(path = "v1")
@@ -65,8 +64,6 @@ public class PersonController {
         }
         return ResponseEntity.ok().body(GenericResponse.Make(RC.OK,dataStore.getDataByKey(id)));
     }
-
-
 
     @GetMapping("person")
     public ResponseEntity<?> getPerson() {
--- api-products/src/main/java/ec/edu/uisrael/apiproducts/controller/ProductController.java
+++ api-products/src/main/java/ec/edu/uisrael/apiproducts/controller/ProductController.java
@@ -1,30 +1,81 @@
 package ec.edu.uisrael.apiproducts.controller;
 
+import ch.qos.logback.classic.util.LogbackMDCAdapter;
 import ec.edu.uisrael.apiproducts.data.FakeData;
 import ec.edu.uisrael.apiproducts.data.Product;
+import lombok.experimental.PackagePrivate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.filter.RequestContextFilter;
+
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 
 @RestController
 @RequestMapping(path = "/v1")
 public class ProductController {
 
+    private final RequestContextFilter requestContextFilter;
+    private FakeData fakeData = FakeData.getInstance();
+
+    public ProductController(RequestContextFilter requestContextFilter) {
+        this.requestContextFilter = requestContextFilter;
+    }
+
     @GetMapping("/product")
     public ResponseEntity<?> getProduct() {
-        FakeData fakeData = FakeData.getInstance();
         return new ResponseEntity<>(fakeData.getData(), HttpStatus.OK);
     }
+
     @GetMapping("/product/{code}")
     public ResponseEntity<?> getProductById(@PathVariable String code) {
-        FakeData fakeData = FakeData.getInstance();
         Product product = fakeData.getDataByKey(code);
         if (product == null) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
         return new ResponseEntity<>(product, HttpStatus.OK);
     }
+
+    @PostMapping("/product")
+    public ResponseEntity<?> addProduct(@RequestBody Product product) {
+        if (product.getId() == null || product.getId().isEmpty()) {
+            product.setId(UUID.randomUUID().toString());
+        }
+
+        fakeData.addData(product);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @PutMapping("/product/{id}")
+    public ResponseEntity<?> updateData(@PathVariable String id, @RequestBody Product product) {
+        if (product.getId() == null || product.getId().isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        fakeData.updateData(id, product);
+        return new ResponseEntity<>("Producto actulizado correctamente",HttpStatus.OK);
+    }
+
+    @PatchMapping("/product/{id}")
+    public ResponseEntity<?> updateDataById(@PathVariable String id, @RequestBody Product product) {
+        if (product.getId() == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        if (product.getName() != null) {
+            product.setName(product.getName());
+        }
+
+        fakeData.updateDataById(id, product);
+        return new ResponseEntity<>("Producto actualizado correctamente",HttpStatus.OK);
+    }
+
+    @DeleteMapping("/product/{id}")
+    public ResponseEntity<?> deleteProduct(@PathVariable String id) {
+        fakeData.removeData(id); // Método para eliminar datos en `fakeData`
+
+        return new ResponseEntity<>("Producto eliminado correctamente", HttpStatus.NO_CONTENT); // Código 204 - Eliminación exitosa
+    }
+
+
 }
